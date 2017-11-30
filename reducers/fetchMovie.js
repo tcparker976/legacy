2bbf8e66fb5660fbf863059272ad5b984976ba4d
messerc <messer55@gmail.com>
2017-11-29 19:29:19 -0800

    minor lifecycle adjustment for moviedetail

--- reducers/fetchMovie.js
+++ reducers/fetchMovie.js
@@ -1,9 +1,11 @@
-import { FETCH_MOVIE } from '../actions/MovieAction';
+import { FETCH_MOVIE, CLEAR_MOVIE } from '../actions/MovieAction';
 
 export default function (state = null, action) {
   switch (action.type) {
     case FETCH_MOVIE:
       return action.payload.data;
+    case CLEAR_MOVIE:
+      return null
     default:
       return state;
   }
