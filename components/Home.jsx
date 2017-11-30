e596a90ea9fd090ba3dd0ef0bedae9a136b20fed
messerc <messer55@gmail.com>
2017-11-29 19:15:40 -0800

    quick restructure on App and added a Home Component

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import SearchBarContainer from '../containers/SearchBarContainer';
 import MovieListContainer from '../containers/MovieListContainer';
 
-const App = (props) => {
+const Home = () => {
   return (
     <div>
-      <nav>NAVBAR</nav>
-      <h1>MarsKliff</h1>
-      {props.children}
+      <SearchBarContainer />
+      <MovieListContainer />
     </div>
   );
 }
 
-export default App; 
+export default Home; 
