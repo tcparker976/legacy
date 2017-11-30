7cf71671853950017e1cbabad449ce3330c58ab1
Thomas Parker <parkerthomas976@gmail.com>
2017-11-29 19:06:22 -0800

    testing new codebase

--- components/MovieList.jsx
+++ components/MovieList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default class MovieList extends Component {
 
