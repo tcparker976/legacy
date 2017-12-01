b1185e862692d950363a9e5e0c60f501f3229d56
David Kumarbai <cygnidavid@gmail.com>
2017-11-30 17:24:50 -0800

    nothing

--- components/App.jsx
+++ components/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import NavBar from './NavBar.jsx';
 
 const App = (props) => {
+  console.log('PROPS CHILDRENITOS')
+  console.log(props.children);
   return (
     <div>
       <NavBar />
