627c98575459c4ee635c6d82d0307dfbdaa6928b
James Terry <jamesnorthterry@gmail.com>
2017-12-02 12:12:22 -0800

    massive database inits

--- databaseInit/index.js
+++ databaseInit/index.js
@@ -4,15 +4,19 @@ const Movie = require('../db/Movie');
 if (!process.env.TMDBAPI) {
   config = require('../config/config.js');
 }
-const omdbToken = process.env.OMDBAPI || config.OMDBAPI;
+const omdbToken = '23f31b33' || process.env.OMDBAPI || config.OMDBAPI;
 
-var json = require('./movieList.json');
+// var json = require('./movieList.json');
+// var json = require('./movieList1980.json');
+var json = require('./movieList1900.json');
+// console.log(json.length);
 
-var lastMovie = json[json.length];
+var totalReadCount = 18167;
 
-var isComplete = (movieObj) => {
-  if (movieObj === lastMovie) {
-    console.log('\x1b[0m', '////////////////////////////////');
+var isComplete = () => {
+  console.log('\x1b[0m' + `${totalReadCount} out of ${json.length - 1}`);
+  if (totalReadCount === json.length - 1) {
+    console.log('\x1b[0m' + '////////////////////////////////');
     console.log('Initialization complete!');
     process.exit();
   }
@@ -26,25 +30,34 @@ const movieDownload = async (movieObj) => {
     try {
       const movie = await Movie.findOneAndUpdate({ Title: movieObj.title }, {});
       if (movie) {
-        console.log('\x1b[32m', `In Db - ${movieObj.title}`);
-        isComplete(movieObj);
+        console.log('\x1b[32m' + `In Db - ${movieObj.title}`);
+        totalReadCount += 1;
+        isComplete();
       } else {
-        setTimeout(async () => {
-          var data = await axios.get(`http://www.omdbapi.com/?apikey=${omdbToken}&t=${movieObj.title}`);
-          if (data.data.Response !== 'False') {
-            var movieOMDB = new Movie(data.data);
-            movieOMDB.save((err, res) => {
-              if (err) {
-                // console.log('\x1b[31m', err);
+        var data = await axios.get(`http://www.omdbapi.com/?apikey=${omdbToken}&t=${movieObj.title}&plot=full`);
+        if (data.data.Response !== 'False') {
+          var movieOMDB = new Movie(data.data);
+          movieOMDB.save((err, res) => {
+            if (err) {
+              if (err.code === 11000) {
+                console.log('\x1b[35m' + `Dupp  - ${movieObj.title}`);totalReadCount += 1;
+                isComplete();
+              } else {
+                console.log('\x1b[31m' + err);
+                totalReadCount += 1;
+                isComplete();
               }
-              isComplete(movieObj);
-            });
-            console.log('\x1b[34m', 'Added -', data.data.Title);
-          } else {
-            console.log('\x1b[33m', `Skip  - ${movieObj.title}`);
-            isComplete(movieObj);
-          }
-        }, 100);
+            } else {
+              totalReadCount += 1;
+              isComplete();
+              console.log('\x1b[34m' + 'Added -', data.data.Title);
+            }
+          });
+        } else {
+          console.log('\x1b[33m' + `Skip  - ${movieObj.title}`);
+          totalReadCount += 1;
+          isComplete();
+        }
       }
     } catch (err) {
       console.log(err);
@@ -56,14 +69,19 @@ const omdbMassiveInitialize = function(starterKey) {
   console.log('starterKey Accepted - Initializing')
   console.log('////////////////////////////////');
   if (starterKey === 'bingoBongo') {
-    for (var i = 0; i < json.length; i++) {
-      movieDownload(json[i])
+    for (var i = 18168; i < json.length; i++) {
+      (function (i) {
+        setTimeout(function () {
+          movieDownload(json[i])
+        }, 5*(i - 18167));
+      })(i);
     }
   } else {
-    console.log('Invalid starterKey');
+    console.log('\x1b[31m' + 'Invalid starterKey');
+    console.log('\x1b[0m' + '////////////////////////////////');
     process.exit()
   }
 }
 
 console.log(`Alert!\nInitializing ${json.length} movies`);
-omdbMassiveInitialize();
+omdbMassiveInitialize('bingoBongo');
